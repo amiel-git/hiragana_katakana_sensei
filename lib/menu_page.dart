eea339a47c22506330ec18d01a60d446c1ba37bf
Amiel <afurongamielchrysdin@gmail.com>
2020-10-05 22:26:13 +0800

    Imported lottie and added a lottie animation on the home page

--- lib/menu_page.dart
+++ lib/menu_page.dart
@@ -1,34 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:hiragana_katakana_sensei/app_button.dart';
 import 'constants.dart';
+import 'package:lottie/lottie.dart';
 
 class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Center(
-            child: Text(
-              'Moji Sensei',
-              style: kTitleTextStyle,),
-          ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Center(
+              child: Text(
+                'Moji Sensei',
+                style: kTitleTextStyle,),
+            ),
+            Lottie.asset('assets/lottiefile.json'),
+            Column(
+              children: <Widget>[
+                AppButton(
+                  child: Text('Hiragana'),
+                ),
+                SizedBox(height: 30),
+                AppButton(
+                  child: Text('Katakana'),
+                )
+              ],
+            ),
 
-          Column(
-            children: <Widget>[
-              AppButton(
-                child: Text('Hiragana'),
-              ),
-              SizedBox(height: 30),
-              AppButton(
-                child: Text('Katakana'),
-              )
-            ],
-          ),
-
-        ],
+          ],
+        ),
       ),
     );
   }
